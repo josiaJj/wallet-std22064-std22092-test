b060d2e39c8d19d96acfe898db916f244ccc434e
josiaJj <hei.josia@gmail.com>
2023-12-05 16:01:28 +0300

    feat: Add Currency model

---
+++ src/main/java/hei/h3/walletheistd22064std22092/Model/Currency.java
@@ -0,0 +1,20 @@
+package hei.h3.walletheistd22064std22092.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Currency {
+    private int id;
+    private String name;
+    private String code;
+}
