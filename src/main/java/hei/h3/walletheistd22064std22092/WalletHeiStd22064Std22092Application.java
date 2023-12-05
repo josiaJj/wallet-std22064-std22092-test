a5a16b4534dc1ac3553465b7c0ab62c6f7520f8c
josiaJj <hei.josia@gmail.com>
2023-12-05 15:46:17 +0300

    initial commit

---
+++ src/main/java/hei/h3/walletheistd22064std22092/WalletHeiStd22064Std22092Application.java
@@ -0,0 +1,13 @@
+package hei.h3.walletheistd22064std22092;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletHeiStd22064Std22092Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletHeiStd22064Std22092Application.class, args);
+    }
+
+}
---
+++ src/test/java/hei/h3/walletheistd22064std22092/WalletHeiStd22064Std22092ApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.h3.walletheistd22064std22092;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletHeiStd22064Std22092ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
