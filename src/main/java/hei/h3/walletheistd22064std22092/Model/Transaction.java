16548229517953ebc15630842803fea5e2821f75
josiaJj <hei.josia@gmail.com>
2023-12-06 14:50:53 +0300

    feat: Add class Transaction

--- src/main/java/hei/h3/walletheistd22064std22092/Model/Transaction.java
+++ src/main/java/hei/h3/walletheistd22064std22092/Model/Transaction.java
@@ -1,4 +1,23 @@
 package hei.h3.walletheistd22064std22092.Model;
 
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Transaction {
+    private int id;
+    private String label;
+    private double amount;
+    private Date dateTime;
+    private String transactionType;
 }
