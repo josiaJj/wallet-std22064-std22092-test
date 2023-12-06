f2ab61c9fa104c09f3788887aa4796919a91f43d
josiaJj <hei.josia@gmail.com>
2023-12-06 15:05:36 +0300

    Add performTransaction method to Account class for manipulating transactions

--- src/main/java/hei/h3/walletheistd22064std22092/Model/Account.java
+++ src/main/java/hei/h3/walletheistd22064std22092/Model/Account.java
@@ -22,4 +22,27 @@ public class Account {
     private List<Transaction> listTransactions = new ArrayList<>();
     private Currency currency;
     private String accountType;
+
+    public Account performTransaction(Transaction transaction) {
+        // Checking balance sufficiency for non-"Bank" accounts on debit transactions
+        if (!accountType.equals("Bank") && transaction.getTransactionType().equals("Debit")) {
+            if (balance < transaction.getAmount()) {
+                System.out.println("Insufficient balance");
+                return null;
+            }
+        }
+
+        // Adding the transaction to the account's transaction list
+        getListTransactions().add(transaction);
+
+        // Updating the account balance based on the transaction type
+        if (transaction.getTransactionType().equals("Credit")) {
+            balance += transaction.getAmount();
+        } else if (transaction.getTransactionType().equals("Debit")) {
+            balance -= transaction.getAmount();
+        }
+
+        // Return the state of the account updated
+        return this;
+    }
 }
